134a8b06df109526c66697ed813832d6cfc5b81e
frieds11 <frieds11@gmail.com>
2015-09-27 20:06:16 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -39,8 +39,24 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch(id)
+        {
+            case R.id.action_todo:
+
+
+                break;
+
+            case R.id.action_settings:
+
+
+                break;
+            
+            case R.id.action_weather:
+
+
+                break;
+
         }
         return super.onOptionsItemSelected(item);
     }
